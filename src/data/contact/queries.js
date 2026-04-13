45c33e0fc7f383ec922701aa24c5e7f8cc4f7ee5
Trevor Rounce <trevor@switchbackcreative.ca>
2026-04-13 12:50:16 -0600

    updated community drop downs

---
+++ src/data/contact/queries.js
@@ -0,0 +1,59 @@
+export const Hero_Query = `
+   	hero: heroComponent {
+        image {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+    }
+`;
+
+export const Intro_Links_Query = `
+   	introLinks: introLinksComponent {
+        title
+        content
+        buttonOneText
+        buttonTwoText
+        buttonOneLink {
+            edges {
+                node {
+                    uri
+                }
+            }
+        }
+        buttonTwoLink {
+            edges {
+                node {
+                    uri
+                }
+            }
+        }
+    }
+`;
+
+export const Address_Query = `
+   	address: addressComponent {
+        address
+    }
+`;
+
+export const Outro_Hero_Query = `
+   	outroHero: outroHeroComponent {
+    content
+        image {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+    }
+`;
