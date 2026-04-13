d61ecb4cdc6d5191e925028682b46ece95e33102
Trevor Rounce <trevor@switchbackcreative.ca>
2026-04-13 10:15:29 -0600

    about page work

---
+++ src/data/destinations/queries.js
@@ -0,0 +1,47 @@
+export const Hero_Query = `
+   	hero: heroComponent {
+        image {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+    }
+`;
+
+export const Intro_Icon_Query = `
+   	introIcon: iconIntroComponent {
+        title
+        content
+        icon {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+    }
+`;
+
+export const Content_Links_Query = `
+   	contentLinks: contentLinksComponent {
+        content
+        buttons {
+            text
+            link {
+                edges {
+                    node {
+                        uri
+                    }
+                }
+            }
+        }
+    }
+`;
