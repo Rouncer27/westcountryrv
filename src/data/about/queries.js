95bd018db630a07de2e2a9a431dda48e1d9b0e3c
Trevor Rounce <trevor@switchbackcreative.ca>
2026-04-13 08:57:23 -0600

    about page work

--- src/data/about/queries.js
+++ src/data/about/queries.js
@@ -36,3 +36,87 @@ export const About_Us_Query = `
         content
     }
 `;
+
+export const Content_Links_Query = `
+   	contentLinks: contentLinksComponent {
+        title
+        content
+        buttons {
+            text
+            link {
+                edges {
+                    node {
+                        uri
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export const About_Bio_Query = `
+   	aboutBio: aboutBioComponent {
+        title
+        bio
+        image {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+    }
+`;
+
+export const Image_Row_Query = `
+   	imageRow: imageRowComponent {
+        imageOne {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        imageTwo {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        imageThree {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+    }
+`;
+
+export const Outro_Hero_Query = `
+   	outroHero: outroHeroComponent {
+        image {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+    }
+`;
